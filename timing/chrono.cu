173afa015575b0eeecd62cedbab9b01ac827d57d
zhangchengzhucufe-dev <zhangchengzhucufe@gmail.com>
2026-09-26 20:52:33 +0800

    update

--- timing/chrono.cu
+++
@@ -1,9 +0,0 @@
-#include <chrono>
-
-int main(){
-    auto start = std::chrono::high_resolution_clock::now();
-    auto end = std::chrono::high_resolution_clock::now();
-    auto duration1 = std::chrono::duration_cast<std::chrono::milliseconds> (end - start);
-    auto duration2 = std::chrono::duration_cast<std::chrono::seconds> (end - start);
-    auto duration3 = std::chrono::duration_cast<std::chrono::microseconds> (end - start);
-}
